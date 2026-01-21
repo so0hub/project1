dc9d4e5d3705554cf4d6893d0ff3804f00c4c565
so0hub <so0mile02@gmail.com>
2026-01-22 00:08:46 +0900

    2026-01-22-12:08

--- js/product.js
+++ js/product.js
@@ -1,14 +1,16 @@
-// 데이터 변환코드
+//defaultProductList / defaultInventories    역할: 공장 초기화용 데이터 (처음 켰을 때만 사용)
+// 즉! 로컬스토리지를 활용하자..
+// 로컬스토리지를 뒤진다.
 
-
-
-// 1. products 변수 설정
 products = JSON.parse(localStorage.getItem("products"));
+
+// 저장소에 아무것도 없을 때 
 if(!products){
-    products=defaultProductList;
-    localStorage.setItem("products",JSON.stringify(products));
+    products=defaultProductList; // 기본데이터를 변수에 넣고,
+    localStorage.setItem("products",JSON.stringify(products)); // 저장소에 저장
 }
-// 2. inventories 변수 설정 (여기가 핵심! 에러 해결 부분)
+
+
 inventories = JSON.parse(localStorage.getItem("inventories"));
 if (!inventories) {
     inventories = defaultInventories;
@@ -16,11 +18,7 @@ if (!inventories) {
 }
 
 
-localStorage.setItem('defaultProductList',JSON.stringify(defaultProductList));
-
-
-
-// [2] 기능 설계
+// [1] 기능 설계
 // 제품 재고 검색 영역 : 제품 목록
 // 재고 조회 함수 , 제품 출력함수(제품명,수량,상태) , 재고 수정 함수
 
@@ -33,22 +31,15 @@ localStorage.setItem('defaultProductList',JSON.stringify(defaultProductList));
 // 실행조건 : 검색 버튼 클릭
 
 
-// 출력 함수 printResult] 
-// 매개변수 : name , stock , status
-// 리턴값 : x(화면에 출력하고 끝)
-// 처리 :
-// (1) HTML에서 결과가 들어갈 자리를 찾는다. ex:id="result-box" 인 곳
-// (2) 그 자리에 매개변수로 받은 name, stock, status를 넣는다.
-
-
-// [3] 제품 영역 구현
-// [3-1] 재고 조회 함수
+// [2] 제품 영역 구현
+// 재고 조회 함수
 
 
 function inventorySearch() {
     
     // (1) 사용자가 입력한 제품명을 가져온다.
     const inputName = document.querySelector(".pNameInput").value;
+
 
     // (2) products 장부에서 그 이름과 일치하는 데이터를 찾아 productCode를 알아낸다.
     let foundProduct = null;
@@ -77,7 +68,7 @@ function inventorySearch() {
 
     // 찾은 데이터를 출력함수로 던져주기
     if (foundInventory != null) {
-        printResult(foundProduct.productName, foundInventory.currentStock, foundInventory.status);
+        printResult([foundInventory]);
     } else {
         alert("재고 정보가 없습니다.");
     }
@@ -86,14 +77,31 @@ function inventorySearch() {
 
 
 
-// [3-2] 출력 함수
-// (4) 찾은 제품명 , 수량 , 상태를 화면에 출력한다.
-printResult();
-function printResult() {
+
+// [1] 기능 설계
+// 출력 함수 printResult] 
+// 매개변수 : dataList
+// 리턴값 : x(화면에 출력하고 끝)
+// 처리 :
+// (1) HTML에서 결과가 들어갈 자리를 찾는다. ex:id="result-box" 인 곳
+// (2) 그 자리에 매개변수로 받은 name, stock, status를 넣는다.
+
+
+// [2] 제품 영역 구현
+// 출력 함수
+// 찾은 제품명 , 수량 , 상태를 화면에 출력한다.
+printResult(inventories.slice(0,5));
+function printResult(dataList) {
+
+    // 만약 datalist가 없으면 기본으로 전체목록(inventories)을 써라. inventories가 로컬 스토리지에서 불러온 수정사항이 반영된 최신 데이터임
+    if(!dataList){dataList=inventories;}
+
     const tbody = document.querySelector("#pdt_list_body");
     let html = ``;
-    for (let index = 0; index <= inventories.length - 1; index++) {
-        let inv = inventories[index];
+
+    // 이제 인벤토리 대신 dataList를 가지고 하자.
+    for (let index = 0; index < dataList.length; index++) {
+        let inv = dataList[index];
 
 
         //제품코드로 제품명으로 바꿈
@@ -157,14 +165,11 @@ function stockUpdate(inventoryCode) {
             const newStatus = selectBox.value; // 사용자가 선택한 값
 
             // inventories데이터장부에 반영
-            inventories[index].currentStock=newInventory;
+            inventories[index].currentStock=Number(newInventory);
             inventories[index].status=newStatus;
 
             localStorage.setItem('inventories',JSON.stringify(inventories));
-           
-
-
-            printResult();
+            printResult(inventories.slice(0,5));
 
             alert("수정되었습니다.")
             break;

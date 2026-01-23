d538eadd8a4b216619dee138e5e228cb100fb49b
so0hub <so0mile02@gmail.com>
2026-01-23 11:08:18 +0900

    product js 취합

--- js/product.js
+++ js/product.js
@@ -1,17 +1,20 @@
+/* [상단 구역 - 박소영]**************************************************************** */
+
+{
 //defaultProductList / defaultInventories    역할: 공장 초기화용 데이터 (처음 켰을 때만 사용)
 // 즉! 로컬스토리지를 활용하자..
 // 로컬스토리지를 뒤진다.
 
-products = JSON.parse(localStorage.getItem("productList"));
+const products = JSON.parse(localStorage.getItem("productList"));
 
 // 저장소에 아무것도 없을 때 
 if(!products){
-    products=defaultMemberList; // 기본데이터를 변수에 넣고,
+    products=defaultProductList; // 기본데이터를 변수에 넣고,
     localStorage.setItem("products",JSON.stringify(products)); // 저장소에 저장
 }
 
 
-inventories = JSON.parse(localStorage.getItem("inventories"));
+const inventories = JSON.parse(localStorage.getItem("inventories"));
 if (!inventories) {
     inventories = defaultInventories;
     localStorage.setItem("inventories", JSON.stringify(inventories));
@@ -32,14 +35,12 @@ if (!inventories) {
 
 
 // [2] 제품 영역 구현
-// 재고 조회 함수
-
+// 재고 조회 함수 inventorySearch]
 
 function inventorySearch() {
     
     // (1) 사용자가 입력한 제품명을 가져온다.
     const inputName = document.querySelector(".pNameInput").value;
-    
 
     // (2) products 장부에서 그 이름과 일치하는 데이터를 찾아 productCode를 알아낸다.
     let foundProduct = null;
@@ -62,7 +63,6 @@ function inventorySearch() {
         if (inventories[j].productCode === foundProduct.productCode) {
             foundInventory = inventories[j];
             break;
-
         } // if END
     } // for END
 
@@ -70,12 +70,8 @@ function inventorySearch() {
     if (foundInventory != null) {
         printResult([foundInventory]);
     } else {
-        alert("재고 정보가 없습니다.");
-    }
-
+        alert("재고 정보가 없습니다.");}
 } // inventorySearch() END
-
-
 
 
 // [1] 기능 설계
@@ -85,7 +81,6 @@ function inventorySearch() {
 // 처리 :
 // (1) HTML에서 결과가 들어갈 자리를 찾는다. ex:id="result-box" 인 곳
 // (2) 그 자리에 매개변수로 받은 name, stock, status를 넣는다.
-
 
 // [2] 제품 영역 구현
 // 출력 함수
@@ -103,7 +98,6 @@ function printResult(dataList) {
     for (let index = 0; index < dataList.length; index++) {
         let inv = dataList[index];
 
-
         //제품코드로 제품명으로 바꿈
         let proname = "";
 
@@ -112,7 +106,6 @@ function printResult(dataList) {
             if(pro.productCode==inv.productCode){
                 proname = pro.productName
                 break;
-
             }
         }
 
@@ -120,20 +113,16 @@ function printResult(dataList) {
         // 영문재고를 한글로 바꿈
         let statuskr = "";
         if (inv.status === "IN_STOCK") {
-            statuskr = "재고있음";
-        }
+            statuskr = "재고있음";  }
         else if (inv.status === "LOW_STOCK") {
-            statuskr = "품절임박";
-        }
+            statuskr = "품절임박";}
         else if (inv.status === "OUT_OF_STOCK") {
-            statuskr = "품절";
-        }
+            statuskr = "품절";}
         else { statuskr = inv.status; }
         
         let selectedIn = (inv.status === "IN_STOCK")?"selected":"";
         let selectedLow = (inv.status === "LOW_STOCK")?"selected":"";
         let selectedOut = (inv.status === "OUT_OF_STOCK")?"selected":"";
-
 
         html += `         <tr>
             <td>${proname}</td>
@@ -151,8 +140,7 @@ function printResult(dataList) {
 }
 
 
-
-// [3-3] 수정 함수
+// 수정 함수 stockUpdate]
 function stockUpdate(inventoryCode) {
     for (let index = 0; index <= inventories.length - 1; index++) {
         if (inventoryCode == inventories[index].inventoryCode) {
@@ -174,5 +162,169 @@ function stockUpdate(inventoryCode) {
             alert("수정되었습니다.")
             break;
         }
+}
+}
+}
+
+
+
+
+
+
+
+
+/* [하단 구역 - 이태현]**************************************************************** */
+{
+/* [1] 초기 기본 데이터: member.js에서 처리
+
+[2] 기능설계
+제품검색목록: 제품 검색, 제품 출력
+- 제품 검색: 제품 검색함수(입력), 제품 출력함수(찾아서 보여주기), 제품 수정함수
+*/
+let obj = [];
+
+// localStorage 활용 productList 호출
+    let products = localStorage.getItem("productList");
+    if( products == null ){
+        products = [];
+    }else{
+        products = JSON.parse(products);
+    }
+
+// [3-1] 검색함수
+function productSearch() { 
+    obj = [];
+
+    // 1. 입력받은 값을 가져온다.
+    const productNameDom = document.querySelector(".m_s_name");
+    const productName = productNameDom.value;
+    const brandDom = document.querySelector(".m_s_b");
+    const brand = brandDom.value;
+    const priceMinDom = document.querySelector(".m_s_min")
+    const priceMin = priceMinDom.value;
+    const priceMaxDom = document.querySelector(".m_s_max");
+    const priceMax = priceMaxDom.value;
+    const categoryDom = document.querySelector(".m_s_k");
+    const categoryCode = categoryDom.value;
+
+    if(productName==""&&brand==""&&priceMin==""&&priceMax==""&&categoryCode=='disabled'){alert("검색하실 상품 정보를 입력하십시오."); }
+
+    // 2. 입력받은 값 찾아서 넣기.
+    for (let i = 0; i < products.length; i++) {
+        let prd = products[i];
+
+        let SearchTrue = true; // 검색 결과가 일치한다고 가정하기
+        
+        if (productName != "" && prd.productName.includes(productName) == false) {
+            SearchTrue = false;
+        }
+        if (brand != "" && prd.brand.includes(brand) == false) {
+            SearchTrue = false;
+        }
+        if (categoryCode != 'disabled' && prd.categoryCode != categoryCode) {
+            SearchTrue = false;
+        }
+        let min = priceMin == "" ? 0 : Number(priceMin);
+        let max = priceMax == "" ? Infinity : Number(priceMax);
+        if (prd.price < min || prd.price > max) {
+            SearchTrue = false;
+        }
+        if (SearchTrue == true) {
+            obj.push(prd);
+        }
+    }
+    productPrint();
+}
+
+//호출
+    // localStorage 활용 categories 호출
+    let categories = localStorage.getItem("categories");
+    if( categories == null ){
+        categories = [];
+    }else{
+        categories = JSON.parse(categories);
+    } 
+    // localStorage 활용 inventories 호출
+    let inventories = localStorage.getItem("inventories");
+    if( inventories == null ){
+        inventories = [];
+    }else{
+        inventories = JSON.parse(inventories);
+    }
+
+// [3-2] 출력함수
+productPrint();
+function productPrint(){
+
+    // 1. 어디에?
+    const tbody = document.querySelector(".m_s_tbody");
+    // 2. 무엇을?
+    let html = ``;
+    for(let i = 0 ; i<= obj.length-1 ; i++){
+        let prd = obj[i];
+        let categoryName = '';
+        for( let j = 0 ; j <= categories.length-1 ; j++){
+            let cat = categories[j];
+            if(cat.categoryCode == prd.categoryCode){
+                categoryName = cat.categoryName;
+            }
+        }
+        let currentStock = '';
+        for( let u = 0 ; u <= inventories.length-1 ; u++){
+            let inv = inventories[u];
+            if(inv.productCode == prd.productCode){
+                currentStock = inv.currentStock;
+            }
+        } 
+
+        html += `<tr>
+                    <td><img src="${prd.img}"/></td>
+                    <td>${prd.productName}</td><td>${categoryName}</td>
+                    <td>${prd.brand}</td><td>${Number(prd.price).toLocaleString()}</td><td>${currentStock}</td>
+                    <td><button onclick="productUpdate('${prd.productCode}')">수정하기</button></td>
+                </tr>`
+    }
+    tbody.innerHTML = html;
+}
+
+// [3-3] 수정함수
+function productUpdate(productCode) {
+    // 1. 수정하고자 하는 정보 찾기
+    let productIndex = 0;
+    for (let i = 0; i < products.length; i++) {
+    if (products[i].productCode === productCode) {
+        productIndex = i;  // 인덱스를 저장했다.
+        break;}
+    }
+    if (productIndex === -1) {
+        alert("해당 상품을 찾을 수 없습니다.");
+        return;}
+
+    // 2. 입력해라
+    const newProductName = prompt("수정할 제품명을 입력하시오.");
+    const newBrand = prompt("수정할 브랜드명을 입력하시오.");
+    const newPrice = prompt("수정할 가격을 입력하시오.");
+
+    // 3. 뭔가 입력했다면 바꾸기
+    if (newProductName !== null){products[productIndex].productName = newProductName;}
+    if (newBrand !== null){products[productIndex].brand = newBrand;}
+    if (newPrice !== null){products[productIndex].price = Number(newPrice);}
+
+    // 4. 로컬에 문자열로 저장
+    localStorage.setItem("productList", JSON.stringify(products));
+
+    // 5. 검색 리스트 동기화
+    let objIndex = 0;
+    for (let i = 0; i < obj.length; i++) {
+        if (obj[i].productCode === productCode) {
+            objIndex = i; // 또 인덱스를 저장했다
+            break;}
+    }
+    obj[objIndex].productName = products[productIndex].productName;
+    obj[objIndex].brand = products[productIndex].brand;
+    obj[objIndex].price = products[productIndex].price;
+    
+    // 6. 새로고침
+    productPrint();
 }
 }
